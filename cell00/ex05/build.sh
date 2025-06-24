9f9a4cf2df8e17ffcaad994e981fa1542ff5213c
Teepiya Piriyaniti <tpiriyan@z2t6c4.42bangkok.com>
2025-06-24 14:44:15 +0700

    fixing 05

--- cell00/ex05/build.sh
+++ cell00/ex05/build.sh
@@ -1,9 +1,14 @@
- for folder_name in "$@"; do
-        mkdir "$new_folder_name"
-        new_folder_name="ex${folder_name}"
-        if [ $? -eq 0 ]; then
-            echo "Created folder: $new_folder_name"
-        else
-            echo "Error creating folder: $new_folder_name"
-        fi
-    done
+ count=0
+
+for folder_name in "$@"; do
+    if [ $count -ge 3 ]; then
+        break
+    fi
+
+    new_folder_name="ex$folder_name"
+    mkdir "$new_folder_name"
+    if [ $? -eq 0 ]; then
+        echo "Created folder: $new_folder_name"
+    else
+        echo "Error creating folder: $new_folder_name"
+    fi
