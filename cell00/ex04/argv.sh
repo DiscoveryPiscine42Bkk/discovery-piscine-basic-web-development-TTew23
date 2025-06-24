dcb081d53dd95b7cb1cf9ffcceea0b45ba257589
Teepiya Piriyaniti <tpiriyan@z2t6c4.42bangkok.com>
2025-06-24 15:02:34 +0700

    OSET

--- cell00/ex04/argv.sh
+++ cell00/ex04/argv.sh
@@ -1,7 +1,12 @@
+count=0
+
 if [ $1 -eq 0 ]; then
     echo "no argument supplied"
-else 
-    for arg in "$@" ; do
+else
+    for arg in "$@"; do
+    if [ $count -ge 3 ]; then
+        break
+    fi
     echo "$arg"
     
     done
